7c1db8003e88f1377b20e5650cc2acef85e8ac10
Arthur <g184811207@gmail.com>
2025-08-11 16:18:55 +0800

    Update abc.cpp

--- operating-systems/thread/abc.cpp
+++ operating-systems/thread/abc.cpp
@@ -1,22 +1,33 @@
-// 交替打印 a b c
-
-#include <iostream>
+#include <mutex>
 #include <thread>
+#include <chrono>
+#include <condition_variable>
 
 using namespace std;
 
-void printChar(char c){
-    cout << c << endl;
+mutex mtx;
+condition_variable cv;
+int count = 0;
+
+void func(int idx, char c){
+    while(1){
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [idx](){return idx == count;});
+        printf("%c", c);
+        count = (++count)%3;
+        cv.notify_all();
+    }
 }
 
+
 int main(){
-    thread t1(printChar, 'a');
-    thread t2(printChar, 'b');
-    thread t3(printChar, 'c');
+    thread t1(func, 0, 'a');
+    thread t2(func, 1, 'b');
+    thread t3(func, 2, 'c');
 
     t1.join();
     t2.join();
     t3.join();
-    
-    return 0;
+
+    printf("\ndone\n");
 }
